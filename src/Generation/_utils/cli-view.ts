c70fda695202e4c958e2d24ca2cf40689a83f66c
dq-mlnck <mlnck@downquark.work>
2022-02-17 22:23:25 -0500

    Web Initted for Deno Integration

--- src/Generation/_utils/cli-view.ts
+++ src/Generation/_utils/cli-view.ts
@@ -10,3 +10,40 @@ export const renderGrid = (Grid:Array<string[]>, _DEBUG = false) => {
   })
   console.log(...bottomBorder)
 }
+
+const runGeneratorSubProcess = async () => {
+  const p = Deno.run({
+    // deno run ./_utils/corridor.ts $(deno run ./_utils/floodfill.ts $(deno run ./CellularAutomata/cellular_automata.ts 20 20 11))
+    cmd: [
+      "deno",
+      "run",
+      "--allow-run",
+      "./CellularAutomata/cellular_automata.ts",
+      ...Deno.args
+      // "./_utils/corridor.ts",
+      // `$(deno run ./_utils/floodfill.ts $(deno run ./CellularAutomata/cellular_automata.ts 20 20 11))`
+    ],
+    stdout: "piped",
+    stderr: "piped",
+  });
+console.log('running', p)
+  const { code } = await p.status();
+console.log('code', code)
+  // Reading the outputs closes their pipes
+  const rawOutput = await p.output();
+  const rawError = await p.stderrOutput();
+
+  if (code === 0) {
+    await Deno.stdout.write(rawOutput);
+  } else {
+    const errorString = new TextDecoder().decode(rawError);
+    console.log(errorString);
+  }
+
+  Deno.exit(code);
+}
+
+// if(Deno.args.length) {
+//   console.log('Running subprocess with: ', Deno.args)
+//   runGeneratorSubProcess()
+// }
