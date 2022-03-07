66a4d87c79626743673eeb32458061a946be0468
dq-mlnck <mlnck@downquark.work>
2022-03-07 01:51:35 -0500

    updates seed handling

--- src/Gaming/RogueLike/development/Browser/Application/rogue/_deno/ui/controls.generators.ts
+++ src/Gaming/RogueLike/development/Browser/Application/rogue/_deno/ui/controls.generators.ts
@@ -2,6 +2,10 @@
 // nethack is 63x18 || 73x19
 import {generateDungeon} from '../generators/dungeon.ts'
 
+const createSeedHash = () => {
+  const hashSeed = new Date().getTime().toString()
+  window.location.hash = hashSeed
+}
 const setCellularAutomataArgs = () => {
   document?.getElementById('generate-button')?.setAttribute('disabled','true')
   const generatorArgs: {[k:string]:number} = { gw: 0, gh: 0, sa: 0, ir: 0, }
@@ -10,7 +14,15 @@ const setCellularAutomataArgs = () => {
     generatorArgs[arg.dataset.genAttr || 'gw'] = parseInt(arg.innerText,10)
   })
 
+  if(!generatorArgs.sa) // update hash in url so we can grab if needed
+  {
+    createSeedHash()
+    generatorArgs.sa = parseInt(window.location.hash.replace('#',''))
+  }
+
   generateDungeon(({...generatorArgs} as { gw: number; gh: number; sa: number; ir: number; }), () => {document?.getElementById('generate-button')?.removeAttribute('disabled')})
 }
-(document.querySelector('span[data-gen-attr="sa"]') as HTMLSpanElement).innerText = new Date().getTime().toString()
+if(!window.location.hash) { createSeedHash() }
+(document.querySelector('span[data-gen-attr="sa"]') as HTMLSpanElement).innerText = window.location.hash.replace('#','')
+
 typeof document !== 'undefined' && document?.getElementById('generate-button')?.addEventListener('click',setCellularAutomataArgs)
