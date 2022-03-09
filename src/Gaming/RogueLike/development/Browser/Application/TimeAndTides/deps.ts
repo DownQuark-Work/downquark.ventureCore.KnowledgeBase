398c8737f3f47d8c86e980712941edf23b780ce6
dq-mlnck <mlnck@downquark.work>
2022-03-09 01:58:42 -0500

    adds rec0mmended services

--- src/Gaming/RogueLike/development/Browser/Application/TimeAndTides/deps.ts
+++ src/Gaming/RogueLike/development/Browser/Application/TimeAndTides/deps.ts
@@ -1,1 +1,7 @@
-export * as Drash from "https://deno.land/x/drash@v2.5.4/mod.ts";
+export * as Drash from 'https://deno.land/x/drash@v2.5.4/mod.ts';
+// 
+export * as unilogger from 'https://deno.land/x/unilogger@v1.0.3/mod.ts';
+export { PaladinService } from 'https://deno.land/x/drash@v2.5.4/src/services/paladin/paladin.ts'
+export { CSRFService } from 'https://deno.land/x/drash@v2.5.4/src/services/csrf/csrf.ts'
+export { ResponseTimeService } from 'https://deno.land/x/drash@v2.5.4/src/services/response_time/response_time.ts'
+export { TengineService } from 'https://deno.land/x/drash@v2.5.4/src/services/tengine/tengine.ts'
