ee212dec6e732c3fc3e53c66300e45bcdcb66178
dq-mlnck <mlnck@downquark.work>
2022-03-08 19:35:25 -0500

    adds websocket instructions

--- src/Gaming/RogueLike/development/Browser/Application/TimeAndTides/app.ts
+++ src/Gaming/RogueLike/development/Browser/Application/TimeAndTides/app.ts
@@ -1,4 +1,4 @@
-// deno run --allow-net --allow-read app.ts # --allow-read is required for loading data from the public/static folder
+// deno run --allow-net --allow-read --allow-write app.ts # --allow-read is required for loading data from the public/static folder
 // $ curl -X POST http://localhost:1447 
 // $ curl -X PUT http://localhost:1447
 // $ curl -X DELETE http://localhost:1447
@@ -12,6 +12,7 @@ import { BodyParsingResource } from './resources/_examples/requests.ts'
 import { StaticFilesResource } from './resources/_examples/static_files_resource.ts'
 import { PrefixedExampleResource as v1PrefixedExampleResource } from './resources/_examples/prefixed/v1/prefix_resource.ts';
 import { PrefixedExampleResource as v2PrefixedExampleResource } from './resources/_examples/prefixed/v2/prefix_resource.ts';
+import { WebSocketResource } from './resources/_examples/web_socket.ts'
 
 import { RequestBodyValidationService } from './services/request_body_validation.ts'
 
@@ -47,6 +48,7 @@ const server = new Drash.Server({
     StaticFilesResource,
     v1PrefixedExampleResource,
     v2PrefixedExampleResource,
+    WebSocketResource,
   ],
   services: [
     new RequestBodyValidationService(),
