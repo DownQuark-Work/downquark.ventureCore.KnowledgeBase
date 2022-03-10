fe1c945229df4e71e0123b8138db486a2d90d884
dq-mlnck <mlnck@downquark.work>
2022-03-09 22:41:23 -0500

    boilerplate deps in place

--- src/Gaming/RogueLike/development/Browser/Application/TimeAndTides/deps.ts
+++ src/Gaming/RogueLike/development/Browser/Application/TimeAndTides/deps.ts
@@ -1,7 +1,9 @@
 export * as Drash from 'https://deno.land/x/drash@v2.5.4/mod.ts';
-// 
-export * as unilogger from 'https://deno.land/x/unilogger@v1.0.3/mod.ts';
-export { PaladinService } from 'https://deno.land/x/drash@v2.5.4/src/services/paladin/paladin.ts'
+
+// services
+export * as unilogger from 'https://deno.land/x/unilogger@v1.0.3/mod.ts'
 export { CSRFService } from 'https://deno.land/x/drash@v2.5.4/src/services/csrf/csrf.ts'
+export { PaladinService } from 'https://deno.land/x/drash@v2.5.4/src/services/paladin/paladin.ts'
+export { RateLimiterService } from "https://deno.land/x/drash@v2.5.4/src/services/rate_limiter/rate_limiter.ts"
 export { ResponseTimeService } from 'https://deno.land/x/drash@v2.5.4/src/services/response_time/response_time.ts'
 export { TengineService } from 'https://deno.land/x/drash@v2.5.4/src/services/tengine/tengine.ts'
