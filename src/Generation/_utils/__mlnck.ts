f5b1053749d87340023b6889d4749ec70399ef29
dq-mlnck <mlnck@downquark.work>
2022-04-30 02:26:23 -0400

    generator stub for walking 2d grid

--- src/Generation/_utils/__mlnck.ts
+++ src/Generation/_utils/__mlnck.ts
@@ -1,3 +1,18 @@
+/* Generator to integrate
+function *walkGridValues() {
+  const colRow = [0,0]
+for (const c of gridReturnObj.Grid) {
+  colRow[1] = 0
+  for (const r of c) {
+      yield [r,colRow]
+      ++colRow[1]
+  }
+  ++colRow[0]
+}
+}
+for(const gridVal of walkGridValues()) {}
+*/
+
 export const ALL_CELLS = (thisArg:any, fnc:any) => {
   console.log('ALL_CELLS >')
   console.log('thisArg', thisArg)
