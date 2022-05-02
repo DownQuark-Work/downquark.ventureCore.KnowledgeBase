83c18f735a7ceb5c892c55b3f4af044637c4d1e0
dq-mlnck <mlnck@downquark.work>
2022-05-02 13:05:30 -0400

    added dynamic filters

--- src/Generation/Narrative/markov.ts
+++ src/Generation/Narrative/markov.ts
@@ -1,12 +1,20 @@
 import { ngrams, ngramths } from './ngram.ts'
 
 const piggy = `This little piggy went to market, This little piggy stayed home, This little piggy had roast beef, This little piggy had none. This little piggy went ... Wee, wee, wee, all the way home`
-const bank = `Break the little box, get the piggy gold Just as much as I can hold Out the door, to the car Oh, together, we’ll go far This is a holdup, momma I was born to be a bank robber Hand over your last gold dollar`
+const bank = `Break the little box, get the piggy gold. Just as much as I can hold. Out the door, to the car. Oh, together, we’ll go far? This is a holdup! momma I was born to be a bank robber. Hand over your last gold dollar!`
 
 const markovChain = () => {
-  // const _ng = ngrams({n:3, src:piggy+bank}),
-  const _ng = ngrams({src:piggy+bank}),
-        _ngth = ngramths
+  const filterShort = (wrd:string) => wrd.replace(/\W/g,'').length > 2
+  const mapBacon = (wrd:string) => wrd.replace('piggy','bacon')
+  const mutateFncs = {
+    filter: [filterShort],
+    map: [mapBacon]
+  }
+  const _ng = ngrams({n:2, src:piggy+' '+bank, mutateFncs}),
+  // const _ng = ngrams({n:3, src:piggy+' '+bank}),
+  // const _ng = ngrams({src:piggy+' '+bank}),
+        _ngth = ngramths()
+  console.log('_ng', _ng)
   console.log('create markov chain'); 
 }
 
--- src/Generation/Narrative/ngram.ts
+++ src/Generation/Narrative/ngram.ts
@@ -1,30 +1,69 @@
-// See ReadMe for deetails
+// See ReadMe for details
 
-export const ngrams = ({n=1, src=''}) => { // n-gram defaults to single word-level (can extend to character level if desired later on)
+type MutateFncsType = {
+  filter: any[],
+  map: any[]
+}
+type MutateFncsTypeType = 'filter'|'map'
+export const ngrams = ({n=1, src='', mutateFncs}:{n:number, src:string, mutateFncs?:MutateFncsType}) => { // n-gram defaults to single word-level
   if (!src.length) return
 
-  const ngram:{[k:string]:{[k:string]:number}} = {}
-  const generateNgram = ({n=1, text =''}) => {
+  let srcArray = src.split(/\s/)
+  function *applyMutateFncs() {
+    for (const mutateType of Object.keys((mutateFncs as MutateFncsType))) {
+      for (const filterFn of (mutateFncs?.[(mutateType as MutateFncsTypeType)] as any[])) {
+        yield (srcArray[(mutateType as MutateFncsTypeType)] as any)(filterFn)
+      }
+    }
+  }
+  if(mutateFncs) {
+    for(const filteredTextArr of applyMutateFncs()){
+      srcArray = (filteredTextArr as unknown as string[])
+    }
+    src = srcArray.join(' ')
+  }
+
+  const getSentenceBookends = ({src = ''}) => [...src.matchAll(/(\w+)[.?!]\s+(\w+)/g)].reduce((a:string[][],c) => {
+    a[0].push(c[1].toLowerCase())
+    a[1].push(c[2].toLowerCase())
+    return [
+      [...new Set(a[0])],
+      [...new Set(a[1])]
+    ]
+  }, [[],[]])
+
+  const generateNgram = ({n=1, text = ''}:{n:number, text:string}) => {
+    const ng:{[k:string]:{[k:string]:number}} = {}
     const cleanTxt = text.replace(/[^\w\s-]|_/g,'').replace(/\s/g,'~!~').replace(/(~!~){2,}/g,'~!~').replace('^~!~','').replace('~!~$','').toLowerCase()
     const txtArr = cleanTxt.split('~!~')
 
     const ngramKey = txtArr.slice(0,n-1)
     ngramKey.unshift('ALLOWS_NON-CONDITIONAL_LOOP')
   
-      for(let indx=n-1; indx<txtArr.length; indx++) {
-        ngramKey.shift()
-        ngramKey.push(txtArr[indx])
+    for(let indx=n-1; indx<txtArr.length; indx++) {
+      ngramKey.shift()
+      ngramKey.push(txtArr[indx])
       const txtKey = ngramKey.join(' ')
       const insertTxt = txtArr[indx+1] || ''
       if(insertTxt.length) {
-        if(!ngram[txtKey]) ngram[txtKey] = {}
-        if(ngram[txtKey]?.[insertTxt]) ngram[txtKey][insertTxt] = ngram[txtKey][insertTxt] + 1
-        else if(!ngram[txtKey]?.[insertTxt]) ngram[txtKey][insertTxt] = 1
+        if(!ng[txtKey]) ng[txtKey] = {}
+        if(ng[txtKey]?.[insertTxt]) ng[txtKey][insertTxt] = ng[txtKey][insertTxt] + 1
+        else if(!ng[txtKey]?.[insertTxt]) ng[txtKey][insertTxt] = 1
       }
     }
-    console.log('ngram', ngram)
+
+    return ng
   }
-  generateNgram({n, text:src})
+  // having the {text:src} hash below if we want to extend/export/etc in the future
+  const ngram = generateNgram({n, text:src})
+  const [sentenceFirsts, sentenceLasts] = getSentenceBookends({src})
+
+  return {
+    ngram,
+    sentenceFirsts, sentenceLasts
+  }
 }
 
-export const ngramths = () => {console.log('ngramths', ngramths)}
+export const ngramths = () => {
+  console.log('TODO: ~~~> DO THIS NOW <~~~')
+}
