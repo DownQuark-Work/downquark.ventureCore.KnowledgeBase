b71b852c04db5f5d98560a3a767f624d0789c4f6
dq-mlnck <mlnck@downquark.work>
2022-07-06 00:35:45 -0400

    validity

--- src/Protocol/bloq/_v0/utils.validity.ts
+++ src/Protocol/bloq/_v0/utils.validity.ts
@@ -1,56 +1,57 @@
-import { BloqType } from "../types.d.ts";
+import { BloqType } from "../types.d.ts"
 
 const isValidTimestamp = (newBlock: BloqType, previousBlock: BloqType): boolean => {
   return ( previousBlock.timestamp - 60 < newBlock.timestamp )
-      && newBlock.timestamp - 60 < new Date().getTime();
-};
+      && newBlock.timestamp - 60 < new Date().getTime()
+}
 
 // const hashMatchesBlockContent = (block: BloqType): boolean => {
-//   const blockHash: string = calculateHashForBlock(block);
-//   return blockHash === block.hash;
-// };
+//   const blockHash: string = calculateHashForBlock(block)
+//   return blockHash === block.hash
+// }
 
 // const hashMatchesDifficulty = (hash: string, difficulty: number): boolean => {
-//   const hashInBinary: string = hexToBinary(hash);
-//   const requiredPrefix: string = '0'.repeat(difficulty);
-//   return hashInBinary.startsWith(requiredPrefix);
-// };
+//   const hashInBinary: string = hexToBinary(hash)
+//   const requiredPrefix: string = '0'.repeat(difficulty)
+//   return hashInBinary.startsWith(requiredPrefix)
+// }
 
 
-// const hasValidHash = (block: BloqType): boolean => {
-//   if (!hashMatchesBlockContent(block)) {
-//       console.log('invalid hash, got:' + block.hash);
-//       return false;
-//   }
+const hasValidHash = (block: BloqType): boolean => {
+  // if (!hashMatchesBlockContent(block)) {
+  //     console.log('invalid hash, got:' + block.hash)
+  //     return false
+  // }
 
-//   if (!hashMatchesDifficulty(block.hash, block.difficulty)) {
-//       console.log('block difficulty not satisfied. Expected: ' + block.difficulty + 'got: ' + block.hash);
-//   }
-//   return true;
-// };
+  // if (!hashMatchesDifficulty(block.hash, block.difficulty)) {
+  //     console.log('block difficulty not satisfied. Expected: ' + block.difficulty + 'got: ' + block.hash)
+  // }
+  return true
+}
+
 const isValidBlockStructure = (block: BloqType): boolean => {
   return typeof block.index === 'number'
       && typeof block.hash === 'string'
       && typeof block.previousHash === 'string'
       && typeof block.timestamp === 'number'
-      && typeof block.data === 'object';
-};
+      && typeof block.data === 'object'
+}
 const isValidNewBlock = (newBlock: BloqType, previousBlock: BloqType): boolean => {
   if (!isValidBlockStructure(newBlock)) {
-      console.log('invalid block structure: %s', JSON.stringify(newBlock));
-      return false;
+      console.log('invalid block structure: %s', JSON.stringify(newBlock))
+      return false
   }
   if (previousBlock.index + 1 !== newBlock.index) {
-      console.log('invalid index');
-      return false;
+      console.log('invalid index')
+      return false
   } else if (previousBlock.hash !== newBlock.previousHash) {
-      console.log('invalid previoushash');
-      return false;
+      console.log('invalid previoushash')
+      return false
   } else if (!isValidTimestamp(newBlock, previousBlock)) {
-      console.log('invalid timestamp');
-      return false;
+      console.log('invalid timestamp')
+      return false
   } else if (!hasValidHash(newBlock)) {
-      return false;
+      return false
   }
-  return true;
+  return true
 }
