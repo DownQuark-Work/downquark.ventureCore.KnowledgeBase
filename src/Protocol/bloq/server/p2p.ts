6bcec44ab5b36bf67b085aca348593f5b4641a3c
dq-mlnck <mlnck@downquark.work>
2022-07-12 01:19:14 -0400

    code cleanup " ;

--- src/Protocol/bloq/server/p2p.ts
+++ src/Protocol/bloq/server/p2p.ts
@@ -26,22 +26,6 @@ export const peers = Object.assign(peersObj, {
 })
   const id = ++peers.peerId;
 
-// let sockets: WebSocket[] = [];
-
-///////////// THE BELOW METHODS SHOULD BE MOCED TO UTILS/WESOCKETS
-// const getSockets = () => sockets;
-// const setSockets = (peers: WebSocket[]) => (sockets = peers);
-
-/* nix
-// const peers = new Map<number, WebSocket>();
-// let peerId = 0;
-// const dispatch = (msg: string): void => {
-//   for (const peer of peers.values()) {
-//     peer.send(msg);
-//   }
-// };
-*/
-
 const JSONToObject = <T>(data: any): T => {
   try {
     return JSON.parse(data as unknown as string);
@@ -49,13 +33,8 @@ const JSONToObject = <T>(data: any): T => {
     console.log(e);
     return null as unknown as T;
   }
-};
+}
 
-///////////// THE BELOW METHODS SHOULD BE MOCED TO UTILS/WESOCKETS
-// const write = (ws: WebSocket, message: MessageType): void => {ws.send(JSON.stringify(message));}
-// const broadcast = (message: MessageType): void => sockets.forEach((socket) => write(socket, message));
-
-// const initMessageHandler = (ws: WebSocket) => {
 export const p2pMessageHandler = (ws: WebSocket, _data:any) => {
   console.log('INIT MESSAGE HANDLER')
   write(ws, queryChainLengthMsg())
@@ -93,22 +72,6 @@ export const p2pMessageHandler = (ws: WebSocket, _data:any) => {
   };
 };
 
-// export const initConnection = (ws: WebSocket) => {
-//   initMessageHandler(ws);
-//   initErrorHandler(ws)
-//   write(ws, queryChainLengthMsg())
-// };
-// export const connectToPeers = (newPeer: string): WebSocket => {
-//   console.log('newPeer', newPeer)
-//   // const ws: WebSocket = new WebSocket(newPeer);
-//   // console.log('ws', ws)
-//   // // ws.onopen = () => {
-//   // //   // initConnection(ws);
-//   // // }
-//   // ws.onerror = () => { console.log('connection failed'); }
-//   // return ws
-// }
-
 const queryAllMsg = (): MessageType => ({'type': enumMessageType.QUERY_ALL, 'data': null})
 const queryChainLengthMsg = (): MessageType => ({'type': enumMessageType.QUERY_LATEST, 'data': null})
 
@@ -121,15 +84,6 @@ const responseLatestMsg = (): MessageType => ({
   type: enumMessageType.RESPONSE_BLOCKCHAIN,
   data: JSON.stringify([getLatestBlock()]),
 });
-
-// const initErrorHandler = (ws: WebSocket) => {
-//   const closeConnection = (wskt: WebSocket) => {
-//     console.log('connection failed to peer: ' + wskt.url)
-//     sockets.splice(sockets.indexOf(wskt), 1)
-//   }
-//   ws.onclose = () => closeConnection(ws)
-//   ws.onerror = () => closeConnection(ws)
-// }
 
 const handleBlockchainResponse = (receivedBlocks: BloqType[]) => {
   console.log('handleBlockchainResponse', handleBlockchainResponse)
@@ -163,16 +117,3 @@ const handleBlockchainResponse = (receivedBlocks: BloqType[]) => {
 }
 
 export const broadcastLatest = (): void => {    broadcast(responseLatestMsg()) }
-
-/*
-// export const p2pHandler = (ws: WebSocket) => {
-//   const id = ++peerId
-//   peers.set(id, ws)
-//   ws.onopen = () => {
-//     initConnection(ws)
-//     dispatch(`Connected: [${id}]`)
-//   }
-// }
-*/
-
-// // export {connectToPeers, broadcastLatest, initP2PServer, getSockets}
